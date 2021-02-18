37d4403f1e44a5f0b93f9a8db7205fa092091d41
Vinit Gajjar <gajjarvineet@hotmail.com>
2021-02-18 21:33:49 +0530

    Major Changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\IncidentCategory;
+use App\Models\IncidentType;
+use App\Models\Location;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        for($i=1;$i<10;$i++){
+
+            IncidentCategory::create([
+                'category'      =>      'Category '.$i,
+            ]);
+
+            IncidentType::create([
+                'type'      =>      'Type '.$i,
+            ]);
+
+            Location::create([
+                'location'      =>      'Location '.$i,
+            ]);
+
+        }
+
     }
 }
